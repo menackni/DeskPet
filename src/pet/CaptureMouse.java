150b836c895015c2a403ec563c6735f90927adab
menackni <90276994+menackni@users.noreply.github.com>
2021-10-22 06:20:03 +0300

    add check that pointer is not null
    application crash when goes into sleep fix.

--- src/pet/CaptureMouse.java
+++ src/pet/CaptureMouse.java
@@ -20,12 +20,13 @@ public class CaptureMouse{
             lastCaptureMove=currentTime;
             if(getPoseHistory().size()>=captureMoveSize-1) getPoseHistory().remove(0);
             PointerInfo pointer=MouseInfo.getPointerInfo();
-            getPoseHistory().add(new Vector2i(pointer.getLocation().x-GLFW_Manager.getDefaultWindow().getPosition().x,pointer.getLocation().y-GLFW_Manager.getDefaultWindow().getPosition().y));//getInput().getMovePos()
-            if(getPoseHistory().get(0).x>GLFW_Manager.getDefaultWindow().getSize().x&&getPoseHistory().get(0).y>GLFW_Manager.getDefaultWindow().getSize().y&&
-                    getPoseHistory().get(0).x<GLFW_Manager.getDefaultWindow().getPosition().x&&getPoseHistory().get(0).y<GLFW_Manager.getDefaultWindow().getPosition().y){
-                GLFW_Manager.getDefaultWindow().getDraw().canDraw(false);
-            }else{
-                GLFW_Manager.getDefaultWindow().getDraw().canDraw(true);
+            if(pointer!=null){
+                getPoseHistory().add(new Vector2i(pointer.getLocation().x-GLFW_Manager.getDefaultWindow().getPosition().x, pointer.getLocation().y-GLFW_Manager.getDefaultWindow().getPosition().y));//getInput().getMovePos()
+                if(getPoseHistory().get(0).x>GLFW_Manager.getDefaultWindow().getSize().x&&getPoseHistory().get(0).y>GLFW_Manager.getDefaultWindow().getSize().y&&getPoseHistory().get(0).x<GLFW_Manager.getDefaultWindow().getPosition().x&&getPoseHistory().get(0).y<GLFW_Manager.getDefaultWindow().getPosition().y){
+                    GLFW_Manager.getDefaultWindow().getDraw().canDraw(false);
+                }else{
+                    GLFW_Manager.getDefaultWindow().getDraw().canDraw(true);
+                }
             }
         }
     }
