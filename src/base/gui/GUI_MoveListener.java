0ba6602551a89930a0d77a403ea1b376ed60bf1b
menackni <90276994+menackni@users.noreply.github.com>
2021-10-22 04:42:37 +0300

    Delete src/base/gui directory

--- src/base/gui/GUI_MoveListener.java
+++
@@ -1,9 +0,0 @@
-package base.gui;
-
-import base.glfw.input.MoveListenerGLFW;
-
-public class GUI_MoveListener extends MoveListenerGLFW {
-    public void move(long window, double x, double y){
-        GUI_Manager.getInput().move(window, x, y);
-    }
-}
